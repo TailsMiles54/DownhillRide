13004a5b3be55980dc2d39e54d1f2f9ff5e4f436
Igor Brusko <tailsmiles2015b@gmail.com>
2024-04-16 15:17:14 +0700

    First

---
+++ Assets/Ilumisoft/Downhill Ride/Assets/Scripts/Highscore/HighscoreSystem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using YG;
+
+namespace Ilumisoft.Game
+{
+    public class HighscoreSystem : MonoBehaviour, IHighscoreSystem
+    {
+        private const string PlayerPrefsKey = "Highscore";
+
+        private float _highscore = 0;
+
+        public float Highscore
+        {
+            get => YandexGame.savesData.MaxScore;
+            set
+            {
+                _highscore = value;
+                SaveHighscore();
+            }
+        }
+
+        private void Awake()
+        {
+            LoadHighscore();
+        }
+
+        private void LoadHighscore()
+        {
+            _highscore = YandexGame.savesData.MaxScore;
+        }
+
+        private void SaveHighscore()
+        {
+            YandexGame.savesData.MaxScore = _highscore;
+            YandexGame.SaveProgress();
+        }
+    }
+}
